fedb364214f0fc3de97a16d98db62976a31e3079
Adrian Hornsby <adhorn@amazon.com>
2021-04-16 08:17:28 +0300

    Adding support for Config Recorder and DeliveryChannel creation

--- app.py
+++ app.py
@@ -7,57 +7,69 @@ from aws_cdk import (
     aws_config as config,
     aws_events_targets as targets,
     aws_cloudtrail as trail,
+    aws_s3 as s3,
     core,
 )
-
-SSH_RULE_NAME = "ssh-restricted"
 
 
 class SshRestrictedStack(core.Stack):
     def __init__(self, app: core.App, id: str) -> None:
         super().__init__(app, id)
 
-        # Create CloulTrail trail
-        awsCloudTrail = trail.Trail(self, 'Trail')
-
-        # Create role for the lambda function
-        awsLambdaSecGroupRole = iam.Role(
-            self, 'aws_lambda_security_group_role',
-            role_name='lambda-security_group_role',
-            assumed_by=iam.ServicePrincipal('lambda.amazonaws.com'),
+        # Setting up ConfigurationRecorder and DeliveryChannel in AWS Config
+        aws_role = iam.Role(
+            self,
+            'ConfigRole',
+            assumed_by=iam.ServicePrincipal('config.amazonaws.com'),
             managed_policies=[
-                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
-            ])
-
-        # Add RunTask policy to Lambda role
-        awsLambdaSecGroupRole.add_to_policy(iam.PolicyStatement(
-            effect=iam.Effect.ALLOW,
-            resources=["*"],
-            actions=["ec2:RevokeSecurityGroupIngress", "config:GetComplianceDetailsByConfigRule", "sts:GetCallerIdentity", "ec2:DescribeSecurityGroups"]))
-
-        # Create lambda function and pass it the above role
-        with open("lambda.py", encoding="utf8") as fp:
-            handler_code = fp.read()
-
-        lambdaFn = lambda_.Function(
-            self, "revoke-ssh-access",
-            role=awsLambdaSecGroupRole,
-            code=lambda_.InlineCode(handler_code),
-            handler="index.lambda_handler",
-            timeout=core.Duration.seconds(300),
-            runtime=lambda_.Runtime.PYTHON_3_7,
+                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSConfigRole")
+            ]
         )
 
-        # Add environment variable for lambda function
-        lambdaFn.add_environment("SSH_RULE_NAME", SSH_RULE_NAME)
+        aws_config_recorder = config.CfnConfigurationRecorder(
+            self,
+            'ConfigRecorder',
+            role_arn=aws_role.role_arn,
+            recording_group={"allSupported": True}
+        )
+
+        aws_config_recorder.node.add_dependency(aws_role)
+
+        aws_config_bucket = s3.Bucket(self, 'ConfigBucket')
+
+        aws_config_bucket.add_to_resource_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            principals=[aws_role],
+            resources=[aws_config_bucket.bucket_arn],
+            actions=["s3:GetBucketAcl", "s3:ListBucket"]
+        ))
+
+        aws_config_bucket.add_to_resource_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            principals=[aws_role],
+            resources=[aws_config_bucket.arn_for_objects('AWSLogs/${cdk.Stack.of(self).account}/Config/*')],
+            actions=["s3:PutObject"],
+            conditions={"StringEquals": {
+                "s3:x-amz-acl": "bucket-owner-full-control"}}
+        ))
+
+        config.CfnDeliveryChannel(
+            self,
+            'ConfigDeliveryChannel',
+            s3_bucket_name=aws_config_bucket.bucket_name
+        )
+
+        # Create CloulTrail trail
+        trail.Trail(self, 'Trail')
 
         # Create Config managed rule
-        config.ManagedRule(
+        aws_config_managed_rule = config.ManagedRule(
             self,
             "restricted-ssh",
-            config_rule_name=SSH_RULE_NAME,
             identifier=config.ManagedRuleIdentifiers.EC2_SECURITY_GROUPS_INCOMING_SSH_DISABLED
         )
+
+        aws_config_managed_rule.node.add_dependency(aws_config_recorder)
 
         # Event pattern triggered by change in the AWS Config compliance rule
         dtl = """{
@@ -78,19 +90,49 @@ class SshRestrictedStack(core.Stack):
         detail = json.loads(dtl)
 
         # Create an eventbridge rule to be triggered by AWS Config
-        rule = events.Rule(
+        aws_event_rule = events.Rule(
             self,
             "Rule",
             description='rule that triggers a lambda function to revoke SSH public access directly after AWS Config NON COMFORM event',
-            rule_name='revoke-access-ssh',
             event_pattern=events.EventPattern(
                 detail=detail,
                 source=["aws.config"]
             )
         )
 
+        # Create role for the lambda function
+        aws_lambda_se_group_role = iam.Role(
+            self,
+            'aws_lambda_security_group_role',
+            assumed_by=iam.ServicePrincipal('lambda.amazonaws.com'),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
+            ])
+
+        # Add policy to Lambda role
+        aws_lambda_se_group_role.add_to_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            resources=["*"],
+            actions=["ec2:RevokeSecurityGroupIngress", "config:GetComplianceDetailsByConfigRule", "sts:GetCallerIdentity", "ec2:DescribeSecurityGroups"]))
+
+        # Create lambda function and pass it the above role
+        with open("lambda.py", encoding="utf8") as fp:
+            handler_code = fp.read()
+
+        aws_lambda_fn = lambda_.Function(
+            self, "revoke-ssh-access",
+            role=aws_lambda_se_group_role,
+            code=lambda_.InlineCode(handler_code),
+            handler="index.lambda_handler",
+            timeout=core.Duration.seconds(300),
+            runtime=lambda_.Runtime.PYTHON_3_7,
+        )
+
+        # Add environment variable for lambda function
+        aws_lambda_fn.add_environment("SSH_RULE_NAME", aws_config_managed_rule.config_rule_name)
+
         # Adding the lambda function as a target of the rule
-        rule.add_target(targets.LambdaFunction(lambdaFn))
+        aws_event_rule.add_target(targets.LambdaFunction(aws_lambda_fn))
 
 
 app = core.App()
